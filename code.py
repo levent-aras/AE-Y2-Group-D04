e03fe14eda43a825c08bf6aa0bc1f0cd499cfc86
Merkkent <fomermank@gmail.com>
2026-03-17 16:22:50 +0100

    Update code.py

--- code.py
+++ code.py
@@ -10,6 +10,7 @@ T =        #temperature
 rho_soot = #soot density
 k_b = 1.38 * 10**(-23)
 m = #mass
+N = 500 # number of points
 #Set the Timesteps and initialize the matrix for mean square displacement
 delta_t = [5, 10, 35, 50]
 
@@ -20,6 +21,9 @@ G = k_b*T/m * (1-math.e**(-2*alpha*delta_t))
 H = (k_b*T/m)*((alpha**(-1))*(1-math.e**(alpha*delta_t)**2))
 I = k_b*T/m * alpha**(-2)*(2*alpha*delta_t-3+4*math.e**(-alpha*delta_t)-math.e**(-2*alpha*delta_t))
 #Call the Gaussian-distributed random numbers
+
+for i in 101:
+  
 
 Y1 = np.random.randn(3)
 Y2 = np.random.randn(3)
