3385c6c1b0cde0ea3662fcb2e2d638fd7ac1eee5
Merkkent <fomermank@gmail.com>
2026-03-17 15:24:32 +0100

    Update code.py

--- code.py
+++ code.py
@@ -23,17 +23,12 @@ I = k_b*T/m * alpha**(-2)*(2*alpha*delta_t-3+4*math.e**(-alpha*delta_t)-math.e**
 
 Y1 = np.random.randn(3)
 Y2 = np.random.randn(3)
-Y3 = np.random.randn(3)
+
 
 #Create arrays for particle velocity and position
-V = [Vx, Vy, Vz]
-R = [Rx, Ry, Rz]
-Vx = Y1*G**0.5
-Vy = Y2*G**0.5
-Vz = Y3*G**0.5
-Rx = Y1*H/G**0.5 + (I-(H**2)/G)**0.5*Y1
-Ry = Y2*H/G**0.5 + (I-(H**2)/G)**0.5*Y2
-Rz = Y3*H/G**0.5 + (I-(H**2)/G)**0.5*Y3
+V = Y1**G**0.5
+R = Y2*H/G**0.5 + (I-(H**2)/G)**0.5*Y2
+
 # I suggest a time loop. For each time step:
 # generate random numbers
 # update velocity & position
