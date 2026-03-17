10a74538c7d8fc103d59254d4e34b5a4c23e3c17
Merkkent <fomermank@gmail.com>
2026-03-17 16:54:17 +0100

    Update code.py

--- code.py
+++ code.py
@@ -35,6 +35,8 @@ for i in range(1, n):
   #Create arrays for particle velocity and position
   V = Y1**G**0.5
   R = Y2*H/G**0.5 + (I-(H**2)/G)**0.5*Y2
+  v[i] = v[i-1]*e**(-alpha*delta_t)+V
+  r[i] = r[i-1]+R+(v[i-1]/alpha)*(1-e**(-alpha*delta_t))
   Pos = 
 
 msd = 
